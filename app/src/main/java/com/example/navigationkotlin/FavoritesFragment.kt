58be25044eb0e1a22d18765fc41d5859854364ed
AliAhmed1973 <eng_aliahmed1973@yahoo.com>
2021-07-30 14:49:03 +0200

    Adding FavoritesFragment

--- app/src/main/java/com/example/navigationkotlin/FavoritesFragment.kt
+++ app/src/main/java/com/example/navigationkotlin/FavoritesFragment.kt
@@ -7,15 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class CountryFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country, container, false)
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
-
 
 }
